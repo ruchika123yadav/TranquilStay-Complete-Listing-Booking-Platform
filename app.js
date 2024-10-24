95bc4a59201f6fb5373dadf5c5017cf9737bed07
ruchika123yadav <145842561+ruchika123yadav@users.noreply.github.com>
2024-10-24 18:40:49 +0530

    add the middlewares

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ const path = require('path')
 const methodOverride= require('method-override')
 const Listing = require('./models/listing.js')
 const ejsMate = require('ejs-mate')
+const asyncWrap=require('./utils/wrapAsync.js')
+const ExpressError=require('./utils/ExpressError.js');
 
 
 const app = express();
 const port = 3000;
 
 
-// some useful setups
+// some setups
 app.set("views engine",'ejs')
 app.set("views",path.join(__dirname,'/views'))
 app.engine('ejs',ejsMate);
 
 app.use(express.static(path.join(__dirname,'/public')))
-app.use(express.urlencoded({extended:true}))
+// app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(methodOverride("_method"))
 
@@ -59,41 +62,73 @@ app.get('/listing/new',(req,res)=>{
     res.render('listings/new.ejs')
 })
 
-// CREATE NEW POST ROUTE
-app.post('/listings' ,async(req,res)=>{
-    const list = new Listing(req.body.listing)
-   await list.save()
+// CREATE NEW POST ROUTE  
+// YHA ASYNC ERROR AA SKTA HAII
+app.post('/listings' ,
+    asyncWrap(async(req,res,next)=>{
+        console.log(req.body);
+        console.log(req.body.listing);
+        if(!req.body.listing){
+            throw new ExpressError(400,"Send valid data for listing") 
+            //400 means bad request gyi hai server pe
+        } 
+    const list = new Listing(req.body.listing) //yha se ham req me se ek list le rhe or ussko fir niche save bhi krwa rhe to hame ush jagaj async error aa skta hai 
+    await list.save()
     //  res.send("Here is my post")
-    res.redirect('listings')
- })
+    res.redirect('/listings')
+    
+   
+ }))
 
 // CREATE THE UPDATE ROUTE (GET)
-app.get('/listing/:id/edit',async(req,res)=>{
+app.get('/listing/:id/edit',
+    asyncWrap(async(req,res)=>{
     let {id} = req.params;
     const listing=await Listing.findById(id);
     res.render('listings/edit.ejs',{listing})
 
-})
+}))
 
 // SHOW ROUTE
-app.get('/listing/:id',async (req,res)=>{
+app.get('/listing/:id',
+    asyncWrap(async (req,res)=>{
     let {id} = req.params;
     const listing=await Listing.findById(id);
     res.render('listings/show.ejs',{listing})
 
-})
+}))
 
 // ROUTE FOR PUT TO UPDATE AFTER THE GET ROUTE
-app.put('/listing/:id',async(req,res)=>{
+app.put('/listing/:id',
+    asyncWrap(async(req,res)=>{
+    
+    if(!req.body.listing){
+        throw new ExpressError(400,"Send valid data for listing")  // ye wha error show hoga jha pee ham as a API call ho just ye tumhe apne website wale pe nhi dikhega hoppscotch pe dikh jayega
+        //400 means bad request gyi hai server pe
+    }
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing})
     res.redirect(`/listing/${id}`);
  
-})
+}))
 
 // ROUTE FOR DELETE THE LISTING
-app.delete('/listing/:id/delete',async(req,res)=>{
+app.delete('/listing/:id/delete',
+    asyncWrap(async(req,res)=>{
     let {id} = req.params;
    await Listing.findByIdAndDelete(id);
    res.redirect('/listings')
- })
+ }))
+
+
+
+
+//  ****************ERROR HANDLERS*****************
+app.all('*',(req,res,next)=>{
+      next(new ExpressError(404,'PAGE NOT FOUND'))
+})
+
+app.use((err,req,res,next)=>{
+    let {statusCode=500,message="Something went wrong"}=err;//ye defualts values haii message ki agr koi message nhi diya print krne ko to ye aa jayega
+    res.status(statusCode).send(message)
+    })
