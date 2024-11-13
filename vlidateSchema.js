bd503e983524bb485c08ff6ddd865632628dd34c
ruchika123yadav <145842561+ruchika123yadav@users.noreply.github.com>
2024-11-13 10:35:54 +0530

    update

--- vlidateSchema.js
+++ vlidateSchema.js
@@ -1,16 +1,20 @@
-const joi = require('joi')
 
-module.exports= joi.object({
-      listing: joi.object({
-      title:joi.string().require(),
-      description:joi.string().require(),
-      location:joi.string().require(),
-      country:joi.string().require(),
-      price:joi.number.require().min(0),
-      image:joi.string().require().allow("",null),
+// yha ha ye ishliye krr rhe hai like maan lo mene api ke through jo request bheji ushme mene koi bhi field me uske datatype
+// ke alawa koi or datatype daal diya wo ye jo niche likha hia usse ha 
+// ye galti rok skte hai ki jitne bhi firls hai unme jo bhi datatype diya gya hia
+// fill krne ko wo hi fill ho or agr galat hua koi bhi firld ka datatype jo bhara hai
+// uske liye error aa jayega
 
-      }).require()
-
-})
+const joi = require('joi');
 
 
+module.exports = joi.object({
+    listing: joi.object({
+        title: joi.string().required(),
+        description: joi.string().required(),
+        location: joi.string().required(),
+        country: joi.string().required(),
+        price: joi.number().required().min(0),
+        image: joi.string().required().allow("", null)
+    }).required()
+});
